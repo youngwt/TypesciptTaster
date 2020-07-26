77fb9edda1eda3a210c11e5399c32a178ccb2771
Wayne Atkinson-Young <youngwt@gmail.com>
2020-07-26 21:02:03 +0100

    Implementated a basic API Controller that accepts a simple object and gives a response. This can be seen in the network response in the dev tools (may need to refresh page)

---
+++ TypesciptTaster/Controllers/OrderApiController.cs
@@ -0,0 +1,27 @@
+﻿using System.Web.Http;
+using TypesciptTaster.Models;
+
+namespace TypesciptTaster.Controllers
+{
+    /// <summary>
+    /// A really simple API Controller that accepts an input
+    /// and gives a reply based on that input
+    /// </summary>
+    [AllowAnonymous]
+    [RoutePrefix("api")]
+    public class OrderApiController : ApiController
+    {
+        /// <summary>
+        /// Echoes a confirmation method to simulate receiving an order
+        /// </summary>
+        /// <param name="order"></param>
+        [HttpPost]
+        [Route("order")]
+        public IHttpActionResult Order(OrderModel order)
+        {
+            var receipt = $"Thank you for your order of {order.Order}";
+
+            return Ok(receipt);
+        }
+    }
+}
---
+++ TypesciptTaster/Models/OrderModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TypesciptTaster.Models
+{
+    /// <summary>
+    /// Really simple model for an order
+    /// </summary>
+    public class OrderModel
+    {
+        public string Order { get; set; }
+    }
+}
